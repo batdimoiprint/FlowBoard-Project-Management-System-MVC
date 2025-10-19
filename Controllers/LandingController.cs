7c37f14efe0304ae6be9f9bbe63e55e12a777444
batdimoiprint <johnkennyreyes21@gmail.com>
2025-10-19 21:54:15 +0800

    Removed kabisote mode

---
+++ Controllers/LandingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlowBoard_Project_Management_System_MVC.Controllers
+{
+    public class LandingController : Controller
+    {
+        public IActionResult getLandingPage()
+        {
+            return View("Landing");
+        }
+    }
+}
--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FlowBoard_Project_Management_System_MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
